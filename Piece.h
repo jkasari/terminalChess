e525cc5242dd5baab25bf76781a924d2d227bc88
Jason <jkasari@outlook.com>
2021-02-28 17:30:52 -0800

    Fill .h file for |Piece| class.

--- Piece.h
+++ Piece.h
@@ -1,11 +1,20 @@
 #ifndef Piece_h
 #define Piece_h
+#include <vector>
+
+using Location = std::pair<int8_t, int8_t>
 
 class Piece {
 
  public:
+  /**
+  * Returns the phsical moves a piece can make. This does factor in the board, but not other peices.
+  */
+  std::vector<Location> potentialMoves(Location);
+
 
  private:
+  bool Color;
 
 };
 
