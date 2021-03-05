cdabf87481ef2ce8f1b261b929f9380470f50803
Jason <jkasari@outlook.com>
2021-03-05 10:51:01 -0800

    Add chess notation markings to ChessBoard display

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -2,17 +2,18 @@
 
 
 std::ostream& operator<<(std::ostream& stream, const ChessBoard& chessBoard) {
-    stream << "+---+---+---+---+---+---+---+---+" << std::endl;
+    stream << "  +---+---+---+---+---+---+---+---+" << std::endl;
     for(int i = 0; i < 8; i++) {
-        stream << "| ";
+        stream << 8 - i << " | ";
         for(int j = 0; j < 8; j++) {
             stream << chessBoard.displayPiece(i, j) << " | ";
         }
         if(i < 7) {
-          stream << std::endl << "|---|---|---|---|---|---|---|---|" << std::endl;
+          stream << std::endl << "  |---|---|---|---|---|---|---|---|" << std::endl;
         }
     }
-    stream << std::endl << "+---+---+---+---+---+---+---+---+" << std::endl;
+    stream << std::endl << "  +---+---+---+---+---+---+---+---+";
+    stream << std::endl << "    A   B   C   D   E   F   G   H" << std::endl;
     return stream;
 }
 
