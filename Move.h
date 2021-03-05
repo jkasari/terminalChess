2100684bdc72668f070bc79b09e39f4decd2779b
Jason <jkasari@outlook.com>
2021-03-04 17:14:49 -0800

    Flesh out |Move.h|
    I added more functions and a couple data members to |Move| class.

--- Move.h
+++ Move.h
@@ -11,12 +11,17 @@ class Move {
    * Creates a move based on a piece and preposed location.
    * This takes the locations in chess notation. It stores them as a |Location|.
    */
-  Move(Piece, char[2], char[2]);
+  Move(std::string);
 
   /**
-   * Returns the moves |Location|;
+   * Returns the |Location| that the |Piece| is moving to.
    */
-  Location getLocation(void);
+  Location getMoveToLocation(void);
+
+  /**
+   * Returns the |Location| that the |Piece| is moving from.
+   */
+  Location getMoveFromLocation(void);
 
   /**
    * Returns the |Piece| in the next move.
@@ -24,8 +29,30 @@ class Move {
   Piece getPiece(void);
 
  private:
-  Piece pieceToMove;
-  Location newLocation;
+  /**
+   * Breaks of the name of the |Piece| off of the string used to create the |Move|.
+   */
+  std::string getPieceName(std::string);
+
+  /**
+   * Takes a string of two chars representing chessnotation and returns a |Location|.
+   */
+  Location notationToLocation(std::string);
+
+  /**
+   * Pulls the second argument out of a string. This argument needs to be chess notation.
+   * Such as "E4" or "B7".
+   */
+  std::string extractFirstLocation(std::string);
+
+  /**
+   * Pulls the second argument out of a string. This argument needs to be in chess notation.
+   * Such as "E4" or "B7".
+   */
+  std::string extractSecondLocation(std::string);
+  std::string pieceToMove;
+  Location moveToLocation;
+  Location moveFromLocation;
 
 };
 
