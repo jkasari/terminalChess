bb98543e2de5e9cd29ff4b4ba8742708e60a1cac
Jason <jkasari@outlook.com>
2021-03-05 12:08:14 -0800

    Implement |Parcer| error codes in game logic.

--- main.cpp
+++ main.cpp
@@ -12,23 +12,33 @@ int main() {
   string moveInput;
   uint32_t cycle = 0;
 
-  while(1) {
-  for(int i = 0; i < 100; ++i) {
-    cout << endl;
-  }
   cout << board << endl;
 
-  if(cycle % 2 == 0) {
-    cout << "Your move white:" << endl;
-  } else {
-    cout << "Your move black:" << endl;
-  }
-  getline(cin, moveInput);
-  nextMove = parsy.parseMove(moveInput);
-  board.executeMove(nextMove);
-  cycle++;
-  }
+  while(1) {
 
+    try {
+      if(cycle % 2 == 0) {
+        cout << "Your move white:" << endl;
+      } else {
+        cout << "Your move black:" << endl;
+      }
+      getline(cin, moveInput);
+      nextMove = parsy.parseMove(moveInput);
+      board.executeMove(nextMove);
+      for(int i = 0; i < 100; ++i) {
+        cout << endl;
+      }
+      cout << board << endl;
+      cycle++;
+    } catch( const ParseError& parseError) {
+      switch (parseError) {
+        case ParseError::InvalidPiece: cout << endl << "!!! Invalid Piece !!!" << endl; break;
+        case ParseError::InvalidLocation: cout << endl << "!!! Invalid Location !!!" << endl; break;
+      }
+      cout << endl << "Moves must be in the following form:" << endl << endl;
+      cout << "     \"Piece A1 A2\"" << endl << endl;
+    }
 
+  }
   return 0;
 }
